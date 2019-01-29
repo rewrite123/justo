547b3f30616aace760f1e78ed703fe573461c6c7
greenman <caasilemroh@gmail.com>
2019-01-29 15:32:53 -0600

    Fixed database mixup

--- html/myProfile.js
+++ html/myProfile.js
@@ -9,16 +9,18 @@ async function main(req){
 	var prom = new Promise(async function(resolve, reject){
 		var obj = {};
 		obj.user = await mainJs.authenticateUser(cookies);
-		obj.postings = await db.postings.find({
-			where: {
-				posting_owner: obj.user.id
-			}
-		});
-		obj.jobs = await db.posting_employees.find({
-			where: {
-				employee: obj.user.id
-			}
-		});
+		if(obj.user){
+			obj.postings = await db.postings.find({
+				where: {
+					posting_owner: obj.user.id
+				}
+			});
+			obj.jobs = await db.posting_employees.find({
+				where: {
+					employee: obj.user.id
+				}
+			});
+		}
 		resolve(obj);
 	});
 	return prom;
